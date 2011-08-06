2d368f3fc50d1d8171c0b7ca85646d445e37e14f
Jake Moffatt <jakeonrails@gmail.com>
2011-08-06 05:20:58 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swapgenius::Application.config.secret_token = 'afe39ef3bb4ba6e1db1d628874bd4e696864e5e377ec6a85186d6873decc7dc0523a3eac528e767638c133d2029d1541359e6c84a3e291cca6dbe4a5dbd82074'
